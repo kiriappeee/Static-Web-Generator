797d750cec6de30bf615ff50c51e432425e90f33
Adnan Issadeen <eddyrox1@hotmail.com>
2013-10-20 21:37:53 +0530

    Add post to index functionality complete
    This is fairly self explanatory. Up to 15 posts (modifiable through a
    global variable) are now added to the index file and when the number
    of posts equals 15 on the index page, the oldest page gets pushed out.
    Next feature set to be completed is to add to the archive after which
    the main set of functionality will be done

--- StaticGen.py
+++ StaticGen.py
@@ -1,12 +1,15 @@
-from BeautifulSoup import BeautifulSoup
+from BeautifulSoup import BeautifulSoup, Tag
 from collections import namedtuple
 from datetime import datetime
 import calendar
 import re
+import os
 
 """Create a named tuple to hold the information about the blog post since the post itself is only information without any methods or need to change the variables within it."""
 
 BlogInfo = namedtuple('BlogInfo', 'postTitle, postContent')
+INDEX_FILE_PATH = r"D:\Programming\Python\mystatic\Static Test Site\index.html"
+MAX_INDEX_POSTS = 15
 
 def getFileInfo(htmlCode):
     """Function takes an appropriately formatted HTML file in and returns a BlogInfo Object"""    
@@ -21,8 +24,9 @@ def getFileInfo(htmlCode):
         if iterPoint and hasattr(iterPoint, 'attrs') and iterPoint.attrs == [('class','post-time')]:
             break
         elif iterPoint:
-            content += str(iterPoint)
-    blogInfo = BlogInfo(header, content)
+            content += str(iterPoint)    
+    blogInfo = BlogInfo(header, BeautifulSoup(content))    
+    return blogInfo
 
 def alterPostTime(htmlCode):
     """function to alter the date of posting in the blog post and returns it as a PostDate Object"""
@@ -47,6 +51,38 @@ def getFileAsSoup(fileName):
     htmlFile.close()
     return htmlCode
 
+def checkIndexPostsLength():
+    """Checks how many posts there are on the index page and returns the number as an integer"""
+    indexFile = open(INDEX_FILE_PATH, 'rb')
+    indexCode = BeautifulSoup(indexFile.read())
+    indexFile.close()
+    return len(indexCode.findAll('div', 'post'))
+
+def insertPost(blogInfo, indexPostLength, fileName):
+    """function to check how many posts are there in the index file and if there are 15 then it removes the oldest post and inserts a new one.
+    if there are less than 15 posts then it simply adds one to the zeroth index"""
+    indexFile = open(INDEX_FILE_PATH, 'rb')
+    indexCode = BeautifulSoup(indexFile.read())
+    indexFile.close()    
+    if indexPostLength == MAX_INDEX_POSTS:
+        indexCode = removeOldestPost(indexCode)    
+    newPost = Tag(BeautifulSoup(), 'div', attrs={'class':'post'})
+    postTitle = Tag(BeautifulSoup(), 'h1')
+    postLink = Tag(BeautifulSoup(), 'a', attrs={'href':'/posts/%s'%os.path.basename(fileName)})
+    postLink.insert(0, blogInfo.postTitle)
+    postTitle.insert(0, postLink)    
+    newPost.insert(0, postTitle)
+    newPost.insert(1, blogInfo.postContent)
+    indexCode.find('div','content').insert(0, newPost)
+    indexFile = open(INDEX_FILE_PATH, 'wb')
+    indexFile.write(indexCode.prettify())
+    indexFile.close()
+
+def removeOldestPost(indexCode):
+    """function to remove the oldest post from the index file"""
+    indexCode.findAll('div','post')[14].extract()
+    return indexCode
+
 def doPost(fileName):
     htmlCode = getFileAsSoup(fileName)
     htmlCode = alterPostTime(htmlCode)
@@ -54,3 +90,4 @@ def doPost(fileName):
     htmlFile.write(str(htmlCode))
     htmlFile.close()
     blogInfo = getFileInfo(htmlCode)
+    insertPost(blogInfo, checkIndexPostsLength(), fileName)
